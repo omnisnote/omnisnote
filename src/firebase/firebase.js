e2a59a1a95dce62549436f905534649dc90f5152
alexa griffin <alexa.griffin42@gmail.com>
2019-03-26 09:09:10 -0400

    heck

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+
+const config = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+}
+
+firebase.initializeApp(config)
+
+export default firebase
